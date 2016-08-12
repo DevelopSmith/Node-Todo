792ea5fb549e339bb81e3327dd07ad3f6cc9c260
DevelopSmith <designsuniverse@gmail.com>
2016-08-11 17:20:15 -0700

    Started Frontend

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,5 @@
 var apiController = require('./apiController');
 var mainController = require('./mainController');
 
-module.exports = {
-	apiController(app); // module.export will ask for it
-	mainController(app); // module.export will ask for it
-}
+apiController(app); // module.export will ask for it
+mainController(app); // module.export will ask for it
